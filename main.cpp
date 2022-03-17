8af27653e27e9495a72e41dfd4d2f9b505fce2a4
dikyrest <16520040@std.stei.itb.ac.id>
2022-03-18 03:46:53 +0700

    command GIVE beres

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
+#include <filesystem>
 #include <map>
 #include "Item.h"
+#include "Inventory.h"
+#include "Slot.h"
 using namespace std;
 
 int main() {
@@ -62,24 +65,23 @@ int main() {
     }
     */
 
-    ifstream indata;
-    string intext, filename;
+    // Container untuk tool dan nontool
     map<string, Tool> toolContainer;
     map<string, NonTool> nonToolContainer;
 
-    // Input nama file
-    cout << "Enter filename: ";
-    cin >> filename;
+    // Inventory pemain
+    Inventory playerInventory;
 
-    // Membuka file
-    indata.open(filename.c_str());
-    if (!indata) {
-        cerr << "Error: File could not be opened" << endl;
-        exit(1);
-    }
+    // Path ke folder config
+    string configPath = "./config";
+    string itemConfigPath = configPath + "/item.txt";
+
+    // Membaca file item config
+    ifstream itemConfigFile(itemConfigPath);
+    string intext;
 
     // Pisahkan item tool dan nontool
-    while (getline(indata, intext)) {
+    while (getline(itemConfigFile, intext)) {
         stringstream ss(intext);
         string name, type, isTool;
         int id;
@@ -96,26 +98,93 @@ int main() {
         }
     }
 
-    // Pembacaan file selesai
-    indata.close();
+    // Pembacaan file item config selesai
+    itemConfigFile.close();
     cout << "EOF reached.." << endl;
+
+    // Membaca file recipe
+    // for (const auto &entry : filesystem::directory_iterator(configPath + "/recipe")) {
+    //     cout << entry.path() << endl;
+    // }
 
     // Membuat iterator
     map<string, Tool>::iterator itrTool;
     map<string, NonTool>::iterator itrNonTool;
 
     // Mencetak seluruh isi map
-    for (itrNonTool = nonToolContainer.begin(); itrNonTool!=nonToolContainer.end(); itrNonTool++) {
-        cout << itrNonTool->first << endl;
-        itrNonTool->second.print();
-        cout << endl;
-    }
+    // cout << "NonTools: " << endl;
+    // for (itrNonTool = nonToolContainer.begin(); itrNonTool!=nonToolContainer.end(); itrNonTool++) {
+    //     cout << itrNonTool->first << endl;
+    //     itrNonTool->second.print();
+    //     cout << endl;
+    // }
 
-    cout << "Tools: " << endl;
-    for (itrTool = toolContainer.begin(); itrTool!=toolContainer.end(); itrTool++) {
-        cout << itrTool->first << endl;
-        itrTool->second.print();
-        cout << endl;
+    // cout << "Tools: " << endl;
+    // for (itrTool = toolContainer.begin(); itrTool!=toolContainer.end(); itrTool++) {
+    //     cout << itrTool->first << endl;
+    //     itrTool->second.print();
+    //     cout << endl;
+    // }
+
+    string command;
+    while (cin >> command) {
+        if (command == "GIVE") {
+            string itemName;
+            int itemQty;
+            cin >> itemName >> itemQty;
+            itrTool = toolContainer.find(itemName);
+            itrNonTool = nonToolContainer.find(itemName);
+            if (itrTool != toolContainer.end()) { // Jika tipe item Tool
+                int countEmpty = playerInventory.countSlotEmpty();
+                if (countEmpty < itemQty) {
+                    cout << "Inventory penuh!" << endl;
+                } else {
+                    itrTool->second.addQuantity(itemQty); // Menambah quantity item di container
+                    for (int i=0; i<itemQty; i++) {
+                        int index = playerInventory.findIndexEmpty();
+                        playerInventory.addQuantityAtIndex(index, 1); // Menambah quantity di slot
+                        playerInventory.setPtrItemAtIndex(index, &itrTool->second); // Menunjuk ke alamat item di container
+                    }
+                }
+            } else { // Jika tipe item NonTool
+                // Temukan semua slot yang berisi item ini
+                int index = playerInventory.findIndexItem(&itrNonTool->second);
+                while (itemQty > 0 && index != -1) {
+                    int remainingQty = 64 - playerInventory.getQuantityAtIndex(index);
+                    if (remainingQty >= itemQty) {
+                        playerInventory.addQuantityAtIndex(index, itemQty);
+                        itrNonTool->second.addQuantity(itemQty);
+                        itemQty = 0;
+                    } else {
+                        playerInventory.addQuantityAtIndex(index, remainingQty);
+                        itrNonTool->second.addQuantity(remainingQty);
+                        itemQty -= remainingQty;
+                        index = playerInventory.findIndexItem(&itrNonTool->second);
+                    }
+                } // itemQty == 0 atau sudah tidak ada yang bisa ditumpuk
+                // Jika masih ada slot kosong
+                index = playerInventory.findIndexEmpty();
+                while (itemQty > 0 && index != -1) {
+                    int QtyToBeAdded;
+                    if (itemQty > 64) {
+                        QtyToBeAdded = 64;
+                        itemQty -= 64;
+                    } else {
+                        QtyToBeAdded = itemQty;
+                        itemQty = 0;
+                    }
+                    playerInventory.addQuantityAtIndex(index, QtyToBeAdded);
+                    playerInventory.setPtrItemAtIndex(index, &itrNonTool->second);
+                    itrNonTool->second.addQuantity(QtyToBeAdded);
+                }
+                // Jika sudah tidak ada slot kosong
+                if (itemQty > 0) {
+                    cout << "Inventory penuh!" << endl;
+                }
+            }
+        } else {
+            playerInventory.print();
+        }
     }
 
     return 0;
